f11e0e035aaebd2a4c056cd03b7c453d74a71826
m-woryna <58751570+m-woryna@users.noreply.github.com>
2020-02-11 22:14:15 +0100

    Add files via upload

---
+++ religia_linki_utf8.php
@@ -0,0 +1,176 @@
+<HTML>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+<style>
+H1 {font-family:sans-serif; font-weight:bold; font-size:20px; color:#1b2a5b;}
+H2 {font-family:sans-serif; font-weight:bold; font-size:16px; color:#1b2a5b;}
+.linki {
+  font-size: medium;
+  font-family: Arial, Helvetica, sans-serif;
+  
+}
+.linki a {
+    font-weight: bold;
+    text-decoration: underline;
+    word-break: break-all;
+ }
+</style>
+
+</HEAD>
+<? // porady ogrodnicze - poziom 1  >
+
+error_reporting(0);
+$domena_aga='http://agaleria.pl/';
+$religia='
+<div class="linki">
+<h1>Katalog stron - Religia : </h1>
+<a HREF="http://ekai.pl/" target="_blank">http://ekai.pl/</A> -  magazyn internetowy Katolickiej Agencji Informacyjnej. Wiadomości związane z Kościołem z Polski, ze świata, z Twojej diecezji.<br>
+<a HREF="http://www.wiara.pl/" target="_blank">http://www.wiara.pl/</A> - informacje, publicystyka, formacja, dyskusje, dla wierzących, poszukujących i niewierzących.<br>
+<a HREF="http://goscniedzielny.wiara.pl/" target="_blank">http://goscniedzielny.wiara.pl/</A> - Tygodnik katolicki - Gość Niedzielny, aktualne wiadomości, archiwum.<br>
+<a HREF="http://malygosc.pl/" target="_blank">http://malygosc.pl/</A> - Mały Gość Niedzielny - dla dzieci - wydanie internetowe.<br>
+<a HREF="http://www.opoka.org.pl/" target="_blank">http://www.opoka.org.pl/</A> - serwis Kościoła Katolickiego w Polsce. Bieżące informacje, adresy, bogata biblioteka dokumentów kościelnych i innych.<br>
+<a HREF="http://www.niedziela.pl/" target="_blank">http://www.niedziela.pl/</A> - wydanie internetowe Tygodnika Katolickiego Niedziela, wiadmości z życia Kościoła katolickiego, edycje diecezjalne, artykuły, wywiady, reportaże.<br>
+<a HREF="http://www.deon.pl/" target="_blank">http://www.deon.pl/</A> - Portal społecznościowo-informacyjny DEON.pl - inicjatywa Towarzystwa Jezusowego (jezuitów) i Wydawnictwa WAM.<br>
+<a HREF="http://www.brewiarz.pl/czytelnia/swieci/alfabet.php3" target="_blank">http://www.brewiarz.pl/czytelnia/swieci/alfabet.php3</A> - Alfabetyczny spis informacji o świętych i błogosławionych. <br>
+<a HREF="http://katolicy.net/" target="_blank">http://katolicy.net/</A> - portal nowoczesnych katolików .<br>
+<a HREF="http://adonai.pl/" target="_blank" title="katalog stron religia, adonai.pl">http://adonai.pl/</A> - Portal poświęcony katolickiemu życiu duchowemu: modlitwy, psalmy, artykuły i kazania.
+<br>
+ <a HREF="http://www.wdrodze.pl/"  target="_blank" title="katalog stron, religia, areopag21.pl ">http://areopag21.pl/</A> - Areopag XXI - rozmowy o Bogu, wierze i religii.<br>
+<a HREF="http://www.opusdei.pl/"  target="_blank" title="katalog stron religia, Opus Dei">http://www.opusdei.pl/</A> - Szukając Boga w życiu codziennym, Opus Dei jest Prałaturą personalną Kościoła Katolickiego, która pomaga zwykłym chrześcijanom szukać świętości w ich pracy i zwykłych okolicznościach.<br><br>
+
+<a HREF="http://tv-trwam.pl/" target="_blank" title="katalog stron religia tv-trwam.pl">http://tv-trwam.pl/</A> - TV Trwam.  Prywatna telewizja Fundacji Lux Veritatis mająca charakter katolicko-narodowy.<br>
+<a HREF="http://www.radiomaryja.pl/" target="_blank" title="katalog stron religia radiomaryja.pl">http://www.radiomaryja.pl/</A> - RadioMaryja.pl - Katolicki Głos w Twoim domu. Rozgłośnia narodowo-katolicka o zasięgu ogólnopolskim i światowym.<br>
+
+<br>
+<a HREF="http://www.wydawnictwowam.pl/" target="_blank" title="katalog stron sklepy internetowe księgarnia wydawnictwowam.pl">http://www.wydawnictwowam.pl/</A> - Wydawnictwo WAM - Kraków. Najstarsze wydawnictwo katolickie w Polsce założone w 1872 roku.<br>
+<br>
+<a HREF="http://www.pielgrzym.com.pl/" target="_blank" title="katalog stron religia pielgrzym.com.pl">http://www.pielgrzym.com.pl/</A> - Informator Pielgrzyma, nieodłączny element dla wszystkich wiernych, którzy nawiedzają polskie sanktuaria.<br>
+<a HREF="http://www.pielgrzymki.katowice.pl/" target="_blank" title="katalog stron religia pielgrzymki.katowice.pl">http://www.pielgrzymki.katowice.pl/</A> - Duszpasterstwo Pielgrzymkowe Archidiecezji Katowickiej.<br>
+<br>
+<a HREF="http://www.przeznaczeni.pl/" target="_blank" title="katalog stron religia przeznaczeni.pl">http://www.przeznaczeni.pl/</A> - Umożliwia wzajemne poznanie osób powołanych do małżeństwa, które oczekują na swoją miłość, a jednocześnie są zaangażowane w praktykowanie wiary.
+<br>
+<br>
+
+<h2><a name="sanktuaria">Sanktuaria i klasztory w Polsce: </a></h2>
+<a HREF="https://sanktuariapolskie.com.pl/" target="_blank">https://sanktuariapolskie.com.pl/</A> - Sanktuaria Polskie.<br>
+ <br>
+<B>ślaskie</B><br>
+<a HREF="http://www.jasnagora.pl/ " target="_blank" title="katalog stron religia jasnagora.pl">http://www.jasnagora.pl/</A> - Sanktuarium Matki Bożej JASNA GÓRA w Częstochowie. (woj. śląskie)<br>
+<a HREF="http://www.piekary.bazylika.katowice.opoka.org.pl/" target="_blank">http://www.piekary.bazylika.katowice.opoka.org.pl/</A> -  Sanktuarium Matki Sprawiedliwości i Miłości Społecznej w Piekarach Śląskich (woj. śląskie).<br>
+<a HREF="http://www.lesniow.pl/" target="_blank">http://www.lesniow.pl/</A> - Leśniów, Sanktuarium Maryjne w Leśniowie, Matka Boża Leśniowska (woj. śląskie).<br>
+<a HREF="http://parafia4.webd.pl/" target="_blank" title="katalog stron religia sanktuarium w Pszowie">http://parafia4.webd.pl/ </A> - Sanktuarium Matki Bożej uśmiechniętej w Pszowie (woj.śląskie)<br>
+<a HREF="https://pl.wikipedia.org/wiki/Bazylika_Narodzenia_Naj%C5%9Bwi%C4%99tszej_Maryi_Panny_w_Pszowie" target="_blank" title="katalog stron religia sanktuarium w Pszowie">Sanktuarium Matki Bożej uśmiechniętej w Pszowie (woj.śląskie) - Wikipedia </A><br>
+<a HREF="http://www.rudy-parafia.pl/" target="_blank" title="katalog stron religia sanktuarium w Rudach">http://www.rudy-parafia.pl/ </A> - Sanktuarium Matki Bożej Pokornej w Rudach (woj.śląskie)<br>
+<a HREF="https://pl.wikipedia.org/wiki/Bazylika_Matki_Bo%C5%BCej_Pokornej_w_Rudach" target="_blank" title="katalog stron religia sanktuarium w Rudach">Sanktuarium Matki Bożej Pokornej w Rudach (woj.śląskie) - Wikipedia </A><br>
+<br>
+
+<B>wielkopolskie</B><br>
+<a HREF="http://www.lichen.pl/" target="_blank">http://www.lichen.pl/</A> - Sanktuarium Maryjne w Licheniu, dzieje Sanktuarium, informacje dla pielgrzymów, apostolaty (woj. wielkopolskie).<br><br>
+
+<B>małopolskie</B><br>
+<a HREF="http://www.kalwaria.eu/" target="_blank">http://www.kalwaria.eu/</A> - Kalwaria Zebrzydowska - Sanktuarium Pasyjno-Maryjne(woj. Małopolskie).<br>
+<a HREF="http://www.pielgrzymka-sanktuarium.pl/" target="_blank">http://www.pielgrzymka-sanktuarium.pl/</A> - ABC pielgrzyma, noclegi, Sanktuarium Łagiewniki, Kraków, serwis dla grup pielgrzymkowych, a w nim: o Sanktuarium Bożego Miłosierdzia w Łagiewnikach (woj. małopolskie).<br>
+<a HREF="http://www.mbludzm.pl/" target="_blank" title="katalog stron agaleria, religia, region podhala sanktuarium w Ludźmierzu">http://www.mbludzm.pl/</a> - Sanktuarium Maryjne w Ludźmierzu.<br>
+<a HREF="http://www.it-jura.pl/pl/klasztor.php?go=alwernia" target="blank" target="blank">http://www.it-jura.pl/pl/klasztor.php?go=alwernia</A> - ALWERNIA - klasztor o.o. Bernardynów i sanktuarium Jezusa Miłosiernego (woj. małopolskie).<br>
+<a HREF="http://www.smbf.pl/index.php" target="_blank">http://www.smbf.pl/index.php</A> - Sanktuarium Matki Bożej Fatimskiej- Zakopane, Krzeptówki.<br>
+<a HREF="http://www.karmelczerna.pl/" target="_blank" title="katalog stron religia, karmelczerna.pl" >http://www.karmelczerna.pl/</A> - Sanktuarium Matki Bożej Szkaplerznej i św. Rafała Kalinowskiego w Czernej (woj. małopolskie).
+ <br>
+<br>
+
+<B>świętokrzyskie</B><br>
+<a HREF="http://www.swietykrzyz.pl/" target="_blank" >http://www.swietykrzyz.pl/</A> - Sanktuarium Relikwii Krzyża Świętego, znajdujące się na Świętym Krzyżu (Łysej Górze) w Górach Świętokrzyskich, Bieliny (woj.Świętokrzyskie)<br>
+<br>
+
+<B>warmińsko-mazurskie</B><br>
+<a HREF="http://www.gietrzwald.3c.pl/" target="_blank">http://www.gietrzwald.3c.pl/</A> - Sanktuarium Maryjne w Gierzwałdzie (woj. warmińsko-mazurskie).<br>
+<a HREF="http://www.swlipka.org.pl/index.php?option=com_content&view=category&layout=blog&id=3&Itemid=2" target="_blank">http://www.swlipka.org.pl/</A> - Sanktuarium Maryjne w Świętej Lipce (woj. warmińsko-maurskie).<br>
+<br>
+
+<B>mazowieckie</B><br>
+<a HREF="http://www.niepokalanow.pl/" target="_blank">http://www.niepokalanow.pl/</A> - Sanktuarium w Niepokalanowie (woj.mazowieckie).<br>
+<a HREF="http://www.czerwinsk.salezjanie.pl/index0.htm" target="_blank">http://www.czerwinsk.salezjanie.pl/index0.htm</A> - Sanktuarium Matki Bożej Czerwińskiej, Pani Mazowsza w Czerwińsku (woj. mazowieckie).<br>
+<br>
+
+<B>podkarpackie</B><br>
+<a HREF="http://www.dukla.bernardyni.pl/" target="_blank">http://www.dukla.bernardyni.pl/</A> - Sanktuarium św. Jana z Dukli (woj.podkarpackie).<br>
+<br>
+
+<B>pomorskie</B><br>
+<a HREF="http://pl.wikipedia.org/wiki/Swarzewo#Sanktuarium" target="_blank">http://pl.wikipedia.org/wiki/Swarzewo#Sanktuarium</A> - Sanktuarium Maryjne Królowej Polskiego Morza w Swarzewie (woj. pomorskie).<br>
+<a HREF="http://www.powiatkoscierski.pl/turystyka/hity_powiatu/koscierskie_sanktuaria" target="_blank">Sanktuaria Kościerskie</A> - Kościerzyna (woj.pomorskie)jako jedyne miasto w Europie posiada w jednej parafii dwa sanktuaria maryjne: sanktuarium Matki Boskiej Kościerskiej -  Królowej Rodzin oraz sanktuarium Matki Boskiej Bolesnej.<br>
+<br>
+<B>dolnośląskie</B><br>
+<a HREF="http://wambierzyce.pl" target="_blank" title="katalog stron religia sanktuarium w Wambierzycach">http://wambierzyce.pl</A> - Sanktuarium Matki Bożej Wambierzyckiej Królowej Rodzin.<br>
+<a HREF=https://pl.wikipedia.org/wiki/Sanktuarium_Wambierzyckiej_Królowej_Rodzin" target="_blank" title="katalog stron religia sanktuarium w Wambierzycach">Sanktuarium Wambierzyckiej Królowej Rodzin</A> - Wikipedia<br>
+<a HREF="http://www.opactwo.eu/" target="_blank" title="katalog stron religia opactwo w Krzeszowie">http://www.opactwo.eu/</A> -  Sanktuarium Matki Bożej Łaskawej w Krzeszowie - EUROPEJSKA PERŁA BAROKU.<br>
+<a HREF="http://www.henrykow.eu/" target="_blank" title="katalog stron religia Klasztor Księgi Henrykowskiej">http://www.henrykow.eu/</A> -  Klasztor Księgi Henrykowskiej,  opactwo Cystersów.
+<br>
+
+<br>
+<a HREF="http://www.madonny.zascianek.pl/zawartosc.html" target="_blank" title="katalog stron religia madonny polskie">http://www.madonny.zascianek.pl/zawartosc.html</A> - Polskie Madonny w sanktuariach maryjnych. Wizerunki, informacje.<br><br>
+<a HREF="http://www.szlakcysterski.org/"  target="_blank" title="katalog stron, religia, szlakcysterski.org">http://www.szlakcysterski.org/</A> - Strona Szlaku Cysterskiego w Polsce. Podróżowanie Szlakiem Cysterskim to nie tylko zwiedzanie klasztorów cysterskich, ale i swoista wyprawa w przeszłość.
+<br>
+<br>
+
+<h2><a name="sanktuaria_zagraniczne">Sanktuaria zagraniczne: </a></h2>
+
+<a HREF="http://www.fatima.pt/portal/index.php?lang=PL" target="_blank">http://www.fatima.pt/portal/index.php?lang=PL</A> - Sanktuarium Matki Bożej Fatimskiej (Portugalia).<br>
+<a HREF="http://en.lourdes-france.org/" target="_blank">http://en.lourdes-france.org/</A> - Sanktuarium Matki Bożej w Lourdes (Francja).<br>
+<a HREF="https://pl.wikipedia.org/wiki/Sanktuarium_Matki_Bo%C5%BCej_w_Lourdes" target="_blank" title="katalog stron religia sanktuarium w Lourdes"> Sanktuarium Matki Bożej w Lourdes (Francja) - Wikipedia </A><br>
+<a HREF="http://saletyni.pl/miejsca-swiete/sanktuarium-w-la-salette" target="_blank">http://saletyni.pl/miejsca-swiete/sanktuarium-w-la-salette</A> - Sanktuarium Matki Bożej Płaczącej w La Salette (Francja).<br>
+<a HREF="https://pl.wikipedia.org/wiki/La_Salette" target="_blank" title="katalog stron religia sanktuarium w La Salette">Sanktuarium Matki Bożej Płaczącej w La Salette (Francja) - Wikipedia </A><br>
+
+<a HREF="http://pl.wikipedia.org/wiki/Bazylika_Matki_Bo%C5%BCej_Wi%C4%99kszej" target="_blank">Santa Maria Maggiore w Rzymie (Włochy).</A><br>
+<a HREF="http://www.mentorella.it/pllibro.html" target="_blank">http://www.mentorella.it/pllibro.html</A> - Sanktuarium w Mentorella (Włochy).<br>
+<a HREF="http://pl.wikipedia.org/wiki/Torreciudad"  target="_blank" title="katalog stron religia, sanktuarium orreciudad">http://pl.wikipedia.org/wiki/Torreciudad</A> - Sanktuaruim Maryjne Torreciudad (Hiszpania)<br>
+<a HREF="http://www.guadalupe.pl/guadalupe/sanktuarium.html" target="_blank">http://www.guadalupe.pl/guadalupe/sanktuarium.html</A> - Sanktuarium Matki Boskiej z Guadalupe (Meksyk).<br>
+<a HREF="http://pl.wikipedia.org/wiki/Bazylika_Niepokalanego_Pocz%C4%99cia_w_Waszyngtonie"   target="_blank">Sanktuarium Niepokalanego Poczęcia w Waszyngtonie (Stany Zjednoczone).</A><br>
+<br>
+
+<h2>Zgromadzenia zakonne: </h2>
+<a HREF="http://www.benedyktyni.pl/" target="_blank">http://www.benedyktyni.pl/</A> - Duchowość zakonu, św. Benedykt i jego Reguła, historia benedyktynów w Polsce i na świecie, medalik św. Benedykta, federacja benedyktyńska, klasztory.<br>
+<a HREF="http://www.boromeuszki.pl/" target="_blank" title="katalog stron religia, Boromeuszki">http://www.boromeuszki.pl/</A> - Kongregacja Sióstr Miłosierdzia św. Karola Boromeusza w Trzebnicy.<br>
+<a HREF="http://www.chrystusowcy.pl/schr/" target="_blank">http://www.chrystusowcy.pl/schr/</A> - Towarzystwo Chrystusowe dla Polonii Zagranicznej.<br>
+<a HREF="http://cystersi.pl/" target="_blank">http://cystersi.pl/</A> - Zakon cystersów, przedstawiciele, Cystersi w Polsce, Cystersi na świecie.<br>
+<a HREF="http://dominikanie.pl/" target="_blank">http://dominikanie.pl/</A> - Polska Prowincja Zakonu Kaznodziejskiego (OO. Dominikanie ).<br>
+
+<a HREF="http://www.franciszkanie.pl/" target="_blank">http://www.franciszkanie.pl/</A> - Serwis informacyjny: aktualności, informacje lokalne, misje, dokumenty, biografia św. Franciszka i bogaty katalog franciszkańskich stron www.<br>
+<a HREF="http://www.kapucyni.pl/" target="_blank" title="katalog stron religia kapucyni">http://www.kapucyni.pl/</A> - Bracia Mniejsi Kapucyni - Pokój i dobro.<br>
+<a HREF="http://www.karmelici.pl/" target="_blank" title="katalog stron religia karmelici">http://www.karmelici.pl/</A> - Zakon Braci Najświętszej Marii Panny z Góry Karmel.<br>
+<a HREF="http://www.salezjanie.pl/" target="_blank" title="katalog stron religia salezjanie">http://www.salezjanie.pl/</A> - Salezjanie w Polsce, aktualności, działalność zakonu, kalendarium, czytelnia, spotkania formacyjne.<br>
+<a HREF="http://www.salwatorianie.pl/"  target="_blank" title="katalog stron, religia, salwatorianie.pl">http://www.salwatorianie.pl/</A> - Polska Prowincja Salwatorianów - Aktualności. Historia Towarzystwa Boskiego Zbawiciela, aktualności, placówki, wydawnistwo, duszpasterstwo powołaniowe, Centrum Formacji Duchowej.
+<br>
+<a HREF="http://www.trynitarze.pl/" target="_blank" title="katalog stron religia trynitarze" >http://www.trynitarze.pl/</A> - Trynitarze, Zakon Trójcy Przenajświętszej.<br>
+<a HREF="http://www.loretanki.pl/" target="_blank">http://www.loretanki.pl/</A> - Zgromadzenie Sióstr Matki Bożej Loretańskiej: Loretanki.pl.<br>
+<br>
+<h2>Archidiecezje: </B></h2>
+<a HREF="http://www.archidiecezja.warszawa.pl/" target="_blank" title="katalog stron region mazowiecki, archidiecezja warszawska" >http://www.archidiecezja.warszawa.pl/</A> - Archidiecezja Warszawska. Historia i aktualności, duszpasterze, instytucje archidiecezjalne, poradnie internetowe.
+<br>
+<a HREF="http://www.diecezja.pl/" target="_blank" title="katalog stron region małopolski, archidiecezja krakowska" >http://www.diecezja.pl/</A> - Archidiecezja Krakowska. Historia diecezji, kalendarium aktualnych wydarzeń, biskupi, życie Kościoła Krakowskiego, dane teleadresowe parafii w Archidiecezji.
+<br>
+<a HREF="http://www.archidiecezja.katowice.pl/" target="_blank" title="katalog stron region śląsk, archidiecezja katowicka">http://www.archidiecezja.katowice.pl/</A> -
+Archidiecezja katowicka. Aktualności, kalendarz liturgiczny, informacje o katedrze, kurii, seminarium duchownym i bibliotece, Caritas diecezjalnej oraz o Wydziale Teologicznym.<br>
+<a HREF="http://www.kuriaczestochowa.pl/" target="_blank" title="katalog stron region śląsk, archidiecezja częstochwska">http://www.kuriaczestochowa.pl/</A> - Archidiecezja Częstochowska.<br><br>
+
+<h2>Wydawnictwa: </h2>
+<a HREF="http://www.wydawnictwowam.pl/" target="_blank" title="katalog stron sklepy internetowe księgarnia wydawnictwowam.pl">http://www.wydawnictwowam.pl/</A> - Wydawnictwo WAM - Kraków. Najstarsze wydawnictwo katolickie w Polsce założone w 1872 roku.<br>
+<a HREF="http://www.wdrodze.pl/"  target="_blank" title="katalog stron, religia, wdrodze.pl ">http://www.wdrodze.pl/</A> - Wydawnictwo Polskiej Prowincji Dominikanów w Poznaniu. Przedstawia dorobek myślicieli zarówno katolickich, jak i prawosławnych, protestanckich.<br>
+<a HREF="http://klubpdp.pl/"  target="_blank" title="katalog stron, religia, klubpdp.pl ">http://www.wdrodze.pl/</A> - Klub PDP księgarnia religijna prawda dobro piękno.<br>
+<br><br>
+
+<h2>Ciekawostki: </B></h2>
+<a HREF="<a HREF="http://www.lichen.pl/" target="_blank">http://www.lichen.pl/</A> - Sanktuarium Maryjne w Licheniu - najwiękasza świątynia w Polsce (woj. wielkopolskie).<br>
+<a HREF="http://www.pomnikchrystusakrola.pl/" target="_blank">http://www.pomnikchrystusakrola.pl/</A> - Świebodzin - największa figura Pana Jezusa na świecie (woj.lubuskie).<br>
+</div>
+
+';
+
+print $religia;
+
+$religia_lamus='
+<a HREF="http://www.britishcouncil.org/kids.htm" target="blank">http://www.britishcouncil.org/kids.htm</A> - strona British Council nauka języka angielskiego dla dzieci, piosenki, gry i wierszyki wspomagające naukę. <br><br>
+';
+
+print '<br>';
+?>
+
