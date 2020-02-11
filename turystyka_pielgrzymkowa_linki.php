63b27625fc88a7b06d5609f5f91c8857a75c93c5
m-woryna <58751570+m-woryna@users.noreply.github.com>
2020-02-11 21:53:00 +0100

    Add files via upload

---
+++ turystyka_pielgrzymkowa_linki.php
@@ -0,0 +1,154 @@
+<HTML>
+<HEAD>
+
+<META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=iso-8859-2">
+<style>
+H1 {font-family:sans-serif; font-weight:bold; font-size:20px; color:#1b2a5b;}
+H2 {font-family:sans-serif; font-weight:bold; font-size:17px; color:#1b2a5b;}
+H3 {font-family:sans-serif; font-weight:bold; font-size:15px; color:#1b2a5b;}
+.linki {
+  font-size: medium;
+  font-family: Arial, Helvetica, sans-serif;
+   line-height: 1.2;
+  
+}
+.linki a {
+    font-weight: bold;
+    text-decoration: underline;
+    word-break: break-all;
+ }
+</style>
+
+</HEAD>
+<? // turystyka-atrakcje.pl, sanktuaria, pielgrzymki, turystyka pielgrzymkowa >
+
+error_reporting(0);
+$domenat='http://turystyka-atrakcje.pl/';
+
+
+$turystyka_pielgrzymkowa='<div class="linki"><br><h2>
+TURYSTYKA PIELGRZYMKOWA, SANKTUARIA - LINKI:  </h2><br>
+
+<IMG SRC="'.$domenat.'foto_opolskie/kamien_slaski119i.jpg" BORDER="1" style="width:96%" align="middle"  ALT="turystyka-atrakcje , Sanktuarium św. Jacka w Kamieniu Śląskim"><br><B>Sanktuarium św. Jacka w Kamieniu Śląskim.</B> <br><br>
+
+<B>Kamień Śląski</B> - miejscowość pielgrzymkowa, miejsce urodzenia św. Jacka Odrowąża (1183-1257), założyciela prowincji polskiej dominikanów. W parku ze starodrzewiem  pałac z XVIII w oraz kaplica zamkowa pod wezwaniem św. Jacka, w miejscu komnaty w której miał się narodzić<br>
+<a HREF="http://www.kamien.biz/" target="_blank">http://www.kamien.biz/</A> - Sanktuarium św. Jacka  Centrum Kultury i Nauki Wydziału Teologicznego Uniwersytetu Opolskiego (woj. opolskie).<br>
+<br> 
+
+a HREF="http://www.patrontravel.pl/" target="_blank" title="turystyka-atrakcje, pielgrzymki, sanktuaria, region podhala sanktuarium w Ludźmierzu">www.patrontravel.pl/</a> - Patron Travel - Franciszkańskie Biuro Turystyczno - Pielgrzymkowe. Biuro prowadyone  przez OO. Franciszkanów. <br><br>
+
+<a HREF="http://www.pielgrzymki.pl/"target="_blank">http://www.pielgrzymki.pl/</A> - Orlando Travel - Biuro podróży - pielgrzymki - Sanktuaria Maryjne, szlakami apostołów i świętych, szlakiem dziedzictwa chrześcijańskiego, ziemia święta, pielgrzymki dla klientów indywidualnych.<br>
+<a HREF="http://www.pielgrzymki.info/" target="_blank">http://www.pielgrzymki.info/</A> - Pielgrzymki:Ziemia Święta,Rzym,Watykan,Egipt,Kenia.<br>
+<a HREF="http://www.frater.pl/" target="_blank">http://www.frater.pl/</A> - FRATER - biuro turystyczno-pielgrzymkowe, pielgrzymki, wycieczki<br>
+
+
+<br>
+<a HREF="http://pielgrzymki.opoka.org.pl/" target="_blank">http://pielgrzymki.opoka.org.pl/</A> - Trasy i aktualna pozycja na mapie grup pielgrzymkowych, poradnik pielgrzyma, zdjęcia.<br>
+<br>  
+<a HREF="http://www.szlakcysterski.org/"  target="_blank" title="katalog stron, religia, szlakcysterski.org">http://www.szlakcysterski.org/</A> - Strona Szlaku Cysterskiego w Polsce. Podróżowanie Szlakiem Cysterskim to nie tylko zwiedzanie klasztorów cysterskich, ale i swoista wyprawa w przeszłość.
+<br><br>
+
+<IMG SRC="'.$domenat.'foto_slask/jasna_gora65i.jpg" BORDER="1" style="width:96%" align="middle"  ALT="turystyka-atrakcje , Sanktuarium Matki Bożej JASNA GÓRA w Częstochowie"><br><B>Sanktuarium Matki Bożej JASNA GÓRA w Częstochowie.</B> <br><br>
+<IMG SRC="'.$domenat.'foto_wielkop/lichen05i.jpg" " BORDER="1" style="width:96%" align="middle"  ALT="turystyka-atrakcje , sanktuarium w Licheniu, Wielkopolska , Polska"><br><B>Sanktuarium w Licheniu</B> </a><br><br>
+
+<h3>SANKTUARIA w Polsce: </h3>
+
+<a HREF="http://www.sanktuaria.info/" target="_blank">http://www.sanktuaria.info/</A> - Polskie sanktuaria.<br><br>
+<B>Województwo śląskie :</B><br>
+<a HREF="http://www.jasnagora.pl/ " target="_blank">http://www.jasnagora.pl/</A> - Sanktuarium Matki Bożej JASNA GÓRA w Częstochowie. (woj. śląskie)
+<br>
+<a HREF="http://www.piekary.bazylika.katowice.opoka.org.pl/" target="_blank">http://www.piekary.bazylika.katowice.opoka.org.pl/</A> -  Sanktuarium Matki Sprawiedliwości i Miłości Społecznej w Piekarach Śląskich (woj. śląskie).<br>
+<a HREF="http://www.lesniow.pl/" target="_blank">http://www.lesniow.pl/</A> - Leśniów, Sanktuarium Maryjne w Leśniowie, Matka Boża Leśniowska (woj. śląskie).<br>
+<br>
+<B>Województwo wielkopolskie :</B><br>
+<a HREF="http://www.lichen.pl/" target="_blank">http://www.lichen.pl/</A> - Sanktuarium Maryjne w Licheniu, dzieje Sanktuarium, informacje dla pielgrzymów, apostolaty (woj. wielkopolskie).<br>
+<br>
+<B>Województwo małopolskie :</B><br>
+
+<a HREF="http://www.kalwaria.eu/" target="_blank">http://www.kalwaria.eu/</A> - Kalwaria Zebrzydowska - Sanktuarium Pasyjno-Maryjne(woj. Małopolskie).<br>
+
+<a HREF="http://www.pielgrzymka-sanktuarium.pl/" target="_blank">http://www.pielgrzymka-sanktuarium.pl/</A> - ABC pielgrzyma, noclegi, Sanktuarium Łagiewniki, Kraków, serwis dla grup pielgrzymkowych, a w nim: o Sanktuarium Bożego Miłosierdzia w Łagiewnikach (woj. małopolskie).<br> 
+
+<a HREF="http://www.mbludzm.pl/" target="_blank" title="turystyka-atrakcje, pielgrzymki, sanktuaria, region podhala sanktuarium w Ludźmierzu">http://www.mbludzm.pl/</a> - <B>Sanktuarium Maryjne w Ludźmierzu.</B><br>
+Miejscowość Ludźmierz znana jest przede wszystkim z otaczanego od lat kultu figury Matki Boskiej Ludźmierskiej - Królowej Podhala wykonanej ok. 1400 roku.  Jest też najstarszą parafią katolicką na Podhalu, której historia sięga XIII wieku. Do kościoła w Ludźmierzu przylega  Ogród Różańcowy -  pięknie zaprojektowany, zadbany i utrzymany - jest pamiątką po  wizycie w sanktuarium ludźmierskim w 1997 roku papieża Jana Pawła II.<br>
+<a HREF="http://pl.wikipedia.org/wiki/Sanktuarium_Maryjne_w_Lud%C5%BAmierzu" target="_blank">Sanktuarium Maryjne w Ludźmierzu - Wikipedia</A> <br>  
+
+<a HREF="http://www.it-jura.pl/pl/klasztor.php?go=alwernia" target="blank" target="blank">http://www.it-jura.pl/pl/klasztor.php?go=alwernia</A> - ALWERNIA - klasztor o.o. Bernardynów i sanktuarium Jezusa Miłosiernego (woj. małopolskie).<br>
+
+<B>Sanktuarium Matki Bożej Fatimskiej na zakopiańskich Krzeptówkach </B>  - kościół pw. Matki Bożej Fatimskiej wzniesiony w latach 19871992. Świątynia została konsekrowana przez Jana Pawła II 7 czerwca 1997 w czasie VI pielgrzymki do Polski.W tzw. Parku Fatimskim na terenie sanktuarium umieszczony został ołtarz, przy którym Jan Paweł II odprawił mszę św. w Zakopanem pod Krokwią 6 czerwca 1997.<br>
+<a HREF="http://www.smbf.pl/index.php" target="_blank" title="turystyka pielgrzymkowa, Fatima">http://www.smbf.pl/index.php</A> - Sanktuarium Matki Bożej Fatimskiej na Krzeptówkach.<br>
+<a HREF="https://pl.wikipedia.org/wiki/Sanktuarium_Matki_Bo%C5%BCej_Fatimskiej_w_Zakopanem" target="_blank">Sanktuarium Matki Bożej Fatimskiej w Zakopanem - Wikipedia</A> <br>
+
+<B>Sanktuarium Matki Bożej Objawiającej Cudowny Medalik  </B> - Zakopane-Olcza. Nowa świątynia z lat 80-tych XX wieku, miejsce warte odwiedzenia, o ciekawej architekturze, malowniczo wkomponowane w tatrzański krajobraz.<br>
+<a HREF="http://www.misjonarze-zakopane.pl/" target="_blank">http://www.misjonarze-zakopane.pl/</A> - Sanktuarium MB Objawiającej Cudowny Medalik (woj. małopolskie).<br> 
+<br>
+
+ <B>Województwo podkarpackie :</B><br>
+<a HREF="http://www.dukla.bernardyni.pl/" target="_blank">http://www.dukla.bernardyni.pl/</A> - Sanktuarium św. Jana z Dukli (woj.podkarpackie).<br>
+<br>
+ <B>Województwo mazowieckie :</B><br>
+<a HREF="http://www.niepokalanow.pl/" target="_blank">http://www.niepokalanow.pl/</A> - Sanktuarium w Niepokalanowie (woj.mazowieckie).<br>
+<a HREF="http://www.czerwinsk.salezjanie.pl/index0.htm" target="_blank">http://www.czerwinsk.salezjanie.pl/index0.htm</A> - Sanktuarium Matki Bożej Czerwińskiej, Pani Mazowsza w Czerwińsku (woj. mazowieckie).<br>
+<br>
+ <B>Województwo opolskie :</B><br>
+ <a HREF="http://www.swanna.com.pl" target="_blank">http://www.swanna.com.pl/</A> - Sanktuarium św. Anny na Górze św. Anny  (woj.opolskie).<br>
+ <a HREF="http://www.swanna.pl/" target="_blank">http://www.swanna.pl/</A> - Góra Św. Anny - duszpasterstwo pielgrzymkowe. Historia sanktuarium i figurki św. Anny, duszpasterstwo młodzieży, dom pielgrzyma, uroczystości, dojazd, fotoreportaże, Bractwo Świętej Anny.<br>
+ <br>
+ 
+ <B>Województwo świętokrzyskie :</B><br>
+ 
+<a HREF="http://www.swietykrzyz.pl/" target="_blank">http://www.swietykrzyz.pl/</A> - Sanktuarium Relikwii Krzyża Świętego, znajdujące się na Świętym Krzyżu (Łysej Górze) w Górach Świętokrzyskich, Bieliny (woj.Świętokrzyskie)<br>
+
+<a HREF="http://www.ostrobramska.pl/" target="_blank" title="turystyka pielgrzymkowa, Ostra Brama">http://www.ostrobramska.pl/</A> - Sanktuarium Matki Bożej Ostrobramskiej w Skarżysku - Kamiennej.<br>
+<br>
+
+ <B>Województwo warmińsko-mazurskie :</B><br>
+ 
+<a HREF="http://www.swlipka.org.pl/index.php?option=com_content&view=category&layout=blog&id=3&Itemid=2" target="_blank">http://www.swlipka.org.pl/</A> - Sanktuarium Maryjne w Świętej Lipce (woj. warmińsko-maurskie).<br>
+<a HREF="http://www.gietrzwald.3c.pl/" target="_blank">http://www.gietrzwald.3c.pl/</A> - Sanktuarium Maryjne w Gierzwałdzie (woj. warmińsko-mazurskie).<br>
+<br>
+ <B>Województwo pomorskie :</B><br>
+<a HREF="http://pl.wikipedia.org/wiki/Swarzewo#Sanktuarium" target="_blank">http://pl.wikipedia.org/wiki/Swarzewo#Sanktuarium</A> - Sanktuarium Maryjne Królowej Polskiego Morza w Swarzewie (woj. pomorskie).<br>
+<a HREF="http://www.powiatkoscierski.pl/turystyka/hity_powiatu/koscierskie_sanktuaria" target="_blank">Sanktuaria Kościerskie</A> - Kościerzyna (woj.pomorskie)jako jedyne miasto w Europie posiada w jednej parafii dwa sanktuaria maryjne: sanktuarium Matki Boskiej Kościerskiej -  Królowej Rodzin oraz sanktuarium Matki Boskiej Bolesnej.<br>
+<br>
+
+<IMG SRC="'.$domenat.'foto_malopol/ludzmierz_ogrod_rozanc12i.jpg" BORDER="1" style="width:96%" align="middle"  ALT="turystyka-atrakcje , sanktuarium w Ludźmierzu, malopolska, Podhale , Polska"><br><B>Sanktuarium Maryjne w Ludźmierzu - Ogród Różańcowy.</B> </a><br><br>
+
+<h3>SANKTUARIA ZAGRANICZNE </H3>
+
+<b>Portugalia </b><br>
+<a HREF="https://pl.wikipedia.org/wiki/Sanktuarium_Matki_Bo%C5%BCej_Fatimskiej"   target="_blank">Sanktuarium Matki Bożej Fatimskiej (Portugalia).</A><br><br>
+
+<b>Francja </b><br>
+<a HREF="https://pl.wikipedia.org/wiki/Sanktuarium_Matki_Bożej_w_Lourde"   target="_blank">Sanktuarium Matki Bożej w Lourdes (Francja).</A><br>
+<a HREF="https://pl.wikipedia.org/wiki/La_Salette"   target="_blank">Sanktuarium w La Salette .</A><br>
+<a HREF="https://pl.wikipedia.org/wiki/Le_Puy-en-Velay"   target="_blank">Le Puy-en-Velay</A> - Znajduje się tam figura Matki Boskiej Czarnej Madonny. Jest to również jeden z trzech początkowych punktów na szlaku Santiago de Compostela. <br><br>
+
+<b>Litwa </b><br>
+<a HREF="https://pl.wikipedia.org/wiki/Ostra_Brama_w_Wilnie"   target="_blank">Ostra Barma w Wilnie (Litwa).</A><br><br>
+
+<b>Hiszpania </b><br>
+<a HREF="http://pl.wikipedia.org/wiki/Droga_%C5%9Bw._Jakuba" target="_blank">Droga św. Jakuba - Wikipedia</A>.<br>
+<a HREF="http://caminodelavida.pl/" target="_blank" title="turystyka pielgrzymkowa, Santiago de Compostella">http://caminodelavida.pl/</A> - Camino de Santiago, a inaczej Szlak świętego Jakuba, czyli wędrówka drogami wiodącymi przez całą Europę do Santiago de Compostela.<br><br>
+
+<b>Włochy </b><br>
+<a HREF="https://pl.wikipedia.org/wiki/Sanktuarium_Santa_Casa_w_Loreto" target="_blank">Papieskie Sanktuarium Większe Świętego Domu Madonny z Loreto (Włochy).</A><br>
+<a HREF="http://pl.wikipedia.org/wiki/Bazylika_Matki_Bo%C5%BCej_Wi%C4%99kszej" target="_blank">Santa Maria Maggiore w Rzymie (Włochy).</A><br>
+<a HREF="https://www.resurrectionist.eu/pl/posuga-zmartwychwstancza/nasze-sanktuaria/sanktuarium-matki-boej-askawej-na-mentorelli-wochy/" target="_blank">https://www.resurrectionist.eu/pl/posuga-zmartwychwstancza/nasze-sanktuaria/sanktuarium-matki-boej-askawej-na-mentorelli-wochy/</A> - Sanktuarium w Mentorella (Włochy).<br><br>
+
+<b>Meksyk </b><br>
+<a HREF="https://pl.wikipedia.org/wiki/Bazylika_Matki_Bo%C5%BCej_z_Guadalupe_w_Meksyku" target="_blank">Bazylika Matki Bożej z Guadalupe w Meksyku</A>.<br>
+<br>
+
+<b> Stany Zjednoczone </b><br>
+<a HREF="http://pl.wikipedia.org/wiki/Bazylika_Niepokalanego_Pocz%C4%99cia_w_Waszyngtonie"   target="_blank">Sanktuarium Niepokalanego Poczęcia w Waszyngtonie (Stany Zjednoczone).</A><br>
+<br>
+
+<a HREF="https://pl.wikipedia.org/wiki/Sanktuaria_maryjne"   target="_blank">Sanktuaria
+ Maryjne.</A><br>
+<br>
+
+</div>';
+
+print $turystyka_pielgrzymkowa;
+?>
